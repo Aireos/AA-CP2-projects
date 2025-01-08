2bf4234217ec96553db3a068df772388c69cc706
Aireos <alex.anderson@ucas-edu.net>
2025-01-08 10:37:34 -0700

    worked on it

--- Financial Calculator/main.py
+++ Financial Calculator/main.py
@@ -1,3 +1,10 @@
 
+choice_input = int(input("1= Goal Tester, 2= Compound Interest Calculator, 3= Budget Allocator, 4= Sale Price Calculator, 5= Tip Calculator"))
 
-choice_input = input("")
+if choice_input == 1:
+    week_or_monthly = int(input("would you like to base it on weekly or monthly deposits?(type 1 for weekly and type 2 for monthly): "))
+    if week_or_monthly == 1:
+        deposit = int(input("How much are you planning on depositing every week?: "))
+        goal = int(input("What is your goal?: "))
+        Goal_Timeline = int(goal/deposit)
+        print("It will take you", Goal_Timeline, "weeks to get to your goal.")
