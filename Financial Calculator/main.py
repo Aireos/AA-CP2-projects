67f58640b01b8fadac6bbe196a2d1d5f5da77792
Aireos <alex.anderson@ucas-edu.net>
2025-01-08 10:51:33 -0700

    worked on it :)

--- Financial Calculator/main.py
+++ Financial Calculator/main.py
@@ -1,10 +1,27 @@
 
-choice_input = int(input("1= Goal Tester, 2= Compound Interest Calculator, 3= Budget Allocator, 4= Sale Price Calculator, 5= Tip Calculator"))
+
+print("1= Goal Tester, 2= Compound Interest Calculator, 3= Budget Allocator, 4= Sale Price Calculator, 5= Tip Calculator")
+choice_input = int(input("what would you like to do?: "))
 
 if choice_input == 1:
     week_or_monthly = int(input("would you like to base it on weekly or monthly deposits?(type 1 for weekly and type 2 for monthly): "))
+
     if week_or_monthly == 1:
         deposit = int(input("How much are you planning on depositing every week?: "))
         goal = int(input("What is your goal?: "))
         Goal_Timeline = int(goal/deposit)
-        print("It will take you", Goal_Timeline, "weeks to get to your goal.")
+        print("It will take you", Goal_Timeline, "week(s) to get to your goal.")
+
+    if week_or_monthly == 2:
+        deposit = int(input("How much are you planning on depositing every month?: "))
+        goal = int(input("What is your goal?: "))
+        goal_timeline = int(goal/deposit)
+        print("It will take you", goal_timeline, "month(s) to get to your goal.")
+
+if choice_input == 2:
+    interest_amount = int(input("How much money is having interest?: "))
+    interest = int(input("What is the interest percentage?: "))
+    interest = interest/100
+
+    interest_amount = interest_amount*interest
+    print("After" month_number, "month you will have:" (interest_amount))
