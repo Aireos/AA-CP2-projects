4cc30fe3257bd343809d8bf000166445ab31f8a4
Aireos <alex.anderson@ucas-edu.net>
2025-01-10 10:32:09 -0700

    finished!

--- Financial Calculator/main.py
+++ Financial Calculator/main.py
@@ -1,24 +1,26 @@
 #Alex Anderson Financial Calculator
 
 
-def interest_calc(interest_amount, interest, month_number):
-    interest_amount = (interest_amount*interest)+interest_amount
-    interest_amount = "{0:.2f}".format(interest_amount)
-    month_number += 1
-    print("After", month_number, "month you will have:", interest_amount)
-    return interest_amount, month_number
-
 def type_fixer(text):
     try:
         if "." in text:
             text = float(text)
+            return text
         else:
             text = int(text)
-        return text
+            return text
     except:
-        print("Invalid input.")
+        print("Invalid input")
         return False
         
+def interest_calc(interest_amount, interest, month_number):
+    interest_amount = (interest_amount*interest)+interest_amount
+    interest_amount = "{0:.2f}".format(interest_amount)
+    interest_amount = type_fixer(interest_amount)
+    month_number += 1
+    print("After", month_number, "month(s) you will have:", interest_amount)
+    return interest_amount, month_number
+
 def main():
     while True:
     
@@ -30,9 +32,11 @@ def main():
         
             if week_or_monthly == 1:
                 deposit = type_fixer((input("How much are you planning on depositing every week?: ")))
-                if deposit == False: continue
+                if deposit == False: 
+                    continue
                 goal = type_fixer((input("What is your goal?: ")))
-                if goal == False: continue
+                if goal == False: 
+                    continue
                     
                 Goal_Timeline = int(goal/deposit)
                 print("It will take you", Goal_Timeline, "week(s) to get to your goal.")
@@ -40,9 +44,11 @@ def main():
 
             if week_or_monthly == 2:
                 deposit = type_fixer((input("How much are you planning on depositing every month?: ")))
-                if deposit == False: continue
+                if deposit == False: 
+                    continue
                 goal = type_fixer((input("What is your goal?: ")))
-                if goal == False: continue
+                if goal == False: 
+                    continue
                     
                 goal_timeline = int(goal/deposit)
                 print("It will take you", goal_timeline, "month(s) to get to your goal.")
@@ -51,74 +57,90 @@ def main():
         if choice_input == 2:
             month_number = 0
             interest_amount = type_fixer(input("How much money is having interest?: "))
-            if interest_amount == False: continue
+            if interest_amount == False: 
+                continue
             interest = type_fixer(input("What is the interest percentage?: "))
-            if interest == False: continue
+            if interest == False: 
+                continue
                 
             interest = interest/100
             while month_number < 10:
                 interest_amount, month_number = interest_calc(interest_amount, interest, month_number)
-                interest_amount = type_fixer(interest_amount)
             continue
 
         
         if choice_input == 3:
+            hundred_p = False
             allocations_list = []
             precentage_total = 0
             budget = type_fixer(input("How much money are you allocating?: "))
-            if budget == False: continue
+            if budget == False: 
+                continue
             allocations = int(input("How many things are you allocating to?: "))
             
             if allocations == 1:
                 print("Just put that much money into it!")
                 continue
                 
-            while allocations > 0:
+            while allocations > 0 and hundred_p == False:
                 allocations -= 1
                 allocation_name = input("What is the name for this allocation?: ")
                 allocation_percentage = int(input("What percentage are you putting into this allocation?: "))
+                if allocation_percentage == 100:
+                    print("Just put that much money into it!")
+                    hundred_p = True
+                    break
                 print()
                 allocation_percentage = allocation_percentage/100
                 allocation_list = [allocation_name, allocation_percentage]
                 allocations_list.append(allocation_list)
-                
+
+            if hundred_p == True:
+                continue
+
             for list in allocations_list:
-                print(list[1])
                 precentage_total += list[1]
                 
             if precentage_total > 1:
                 print("You can't have over 100%!")
                 continue
+
+            if precentage_total < 1:
+                print("You can't have under 100%!")
+                continue
                 
             for list in allocations_list:
                 allocation_amount = list[1]*budget
-                list += allocation_amount
+                list = list.append(allocation_amount)
                 
             for list in allocations_list:
-                print("You will be putting", list[2], "in", list[0])
+                print("You will be putting", type_fixer(list[2]), "in", type_fixer(list[0]))
             continue
 
         
         if choice_input == 4:
             original_price = type_fixer(input("What is the original price for the item?: "))
-            if original_price == False: continue
+            if original_price == False: 
+                continue
             discount_percentage = type_fixer(input("What is your total percentage off?: "))
-            if discount_percentage == False: continue
-                
+            if discount_percentage == False:
+                continue                
             discount_percentage = discount_percentage/100
-            final_price = original_price*discount_percentage
+            final_price = original_price - (original_price*discount_percentage)
             print("The price of the product is", final_price)
             continue
 
         
         if choice_input == 5:
             before_tip = type_fixer(input("What was the orginal price?: "))
-            if before_tip == False: continue
+            if before_tip == False: 
+                continue
             tip_percentage = type_fixer(input("What is the precentage you want to tip?: "))
-            if tip_percentage == False: continue
+            if tip_percentage == False: 
+                continue
                 
             tip_percentage = tip_percentage/100
-            after_tip = tip_percentage*before_tip
+            after_tip = before_tip + (tip_percentage*before_tip)
             print("The final price will be", after_tip)
             continue
 
