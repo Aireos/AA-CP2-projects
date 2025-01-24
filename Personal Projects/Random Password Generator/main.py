12d127ee5c1f26a45c2c4835664dd29b32eb2c3e
Aireos <alex.anderson@ucas-edu.net>
2025-01-24 10:11:09 -0700

    added comments

--- Personal Projects/Random Password Generator/main.py
+++ Personal Projects/Random Password Generator/main.py
@@ -1,5 +1,6 @@
 #Alex Anderson, Random Password Generator
 
+#importing random to make randomized passwords and making lists for all the different types of charecters or numbers
 import random
 special_charecters = ["@", "#", "$", "%", "&", "!"]
 uppercase_charecters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -7,6 +8,7 @@ lowercase_charecters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
 numbers = ["1","2","3","4","5","6","7","8","9"]
 
 
+#used for finding the number of uppercase letters and adding uppercase letters to the password
 def uppercase_finder_amount(type, password, uppercase):
     if type == 0:
         while True:
@@ -24,6 +26,7 @@ def uppercase_finder_amount(type, password, uppercase):
         return password, uppercase
 
 
+#used for finding the number of lowercase letters and adding lowercase letters to the password
 def lowercase_finder_amount(type, password, lowercase):
     if type == 0:
         while True:
@@ -40,7 +43,7 @@ def lowercase_finder_amount(type, password, lowercase):
         password += lowercase_charecters[random.randint(0,25)]
         return password, lowercase
 
-
+#used for finding the number of numbers and adding numbers to the password
 def number_finder_amount(type, password, number_amount):
     if type == 0:
         while True:
@@ -57,7 +60,7 @@ def number_finder_amount(type, password, number_amount):
         password += numbers[random.randint(0,8)]
         return password, number_amount
     
-
+#used for finding the number of special charecters and adding special charecters to the password
 def special_finder_amount(type, password, special):
     if type == 0:
         while True:
@@ -74,7 +77,7 @@ def special_finder_amount(type, password, special):
         password += special_charecters[random.randint(0,5)]
         return password, special
 
-
+#used for randomizing what the next charecter is and making sure to use the correct amounts
 def password_generator(uppercase, lowercase, number_amount, special, password):
     password = ""
     while True:
@@ -92,7 +95,7 @@ def password_generator(uppercase, lowercase, number_amount, special, password):
         else:
             continue
 
-
+#has all the stuff that the user can see and what will be the end of the program when done
 def main():
     while True:
         leave = input("Do you want to leave? (yes or no): ")
@@ -151,9 +154,10 @@ def main():
         password = password_generator(uppercase_three, lowercase_three, number_amount_three, special_three, password)
         print("Your fourth password is:", password)
 
-
+#runs main
 main()
 
+#just some stuff to blank screen and say have a good day in center
 for number in range(30):
     print()
 
