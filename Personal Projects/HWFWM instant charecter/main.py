a8b1b8b2e8b27474eb01e65b98618c8cc9260f18
Alex <alex.anderson@ucas-edu.net>
2025-01-28 16:01:22 -0700

    did stuff

--- Personal Projects/HWFWM instant charecter/main.py
+++ Personal Projects/HWFWM instant charecter/main.py
@@ -1,4 +1,10 @@
 name = input("Real life name: ")
 charecter_name = input("Charecter name: ")
-race = input("Race: ")
-
+race = input("Race (elf, human, outworlder, ): ")
+while True:
+    type = input("Type (glass cannon, healer, tank, bruiser, speedster, generalist, debuffer, buffer): ")
+    if type == "glass cannon" or "healer" or "tank" or "bruiser" or "speedster" or "generalist" or "debuffer" or "buffer":
+        break
+    else:
+        print("invalid type")
+        print()
