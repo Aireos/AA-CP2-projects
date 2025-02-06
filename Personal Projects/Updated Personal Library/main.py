046a4c237ccd3359912704c5e2585d754416aa7e
Alex <alex.anderson@ucas-edu.net>
2025-02-05 17:43:43 -0700

    imported home code

--- Personal Projects/Updated Personal Library/main.py
+++ Personal Projects/Updated Personal Library/main.py
@@ -3,7 +3,16 @@
 #used before every other function to have the function know what the song is they are trying to add, remove, etc.
 def making():
     song_name = input("what is the name of the song?: ")
-    song_auther = input("who wrote the song?: ")
+    song_composer = input("who wrote the song?: ")
+    song_album = input("What is the album that the song is in?: ")
+
+    while True:
+        try:
+            song_length = int(input("What is the length in seconds of the song?: "))
+        except:
+            print("invalid input")
+            continue
+        break
 
     while True:
         try:
@@ -13,30 +22,29 @@ def making():
             continue
         break
 
-    song = (song_name,song_auther,song_age)
+    song = song_name[song_composer, song_length, song_album, song_age]
     return song
 
-
 #pretty straight forward, just adds the song to the list of all of their songs.
-def adding(song,song_list):
-    song_list.add(song)
-    return song_list
+def adding(song,song_dictionary):
+    song_dictionary.add(song)
+    return song_dictionary
 
 
-#makes sure the song is in the song_list and then removes it if it is.
-def removing(song,song_list):
-    if song in song_list:
-        song_list.remove(song)
-        return song_list
+#makes sure the song is in the song_dictionary and then removes it if it is.
+def removing(song,song_dictionary):
+    if song in song_dictionary:
+        song_dictionary.remove(song)
+        return song_dictionary
     
     else:
         print("invalid song")
-        return song_list
+        return song_dictionary
 
 
-#also straight forward, just checks if the song is in the song_list and returns false if it is not and returns true if it is.
-def finding(song,song_list):
-    if song in song_list:
+#also straight forward, just checks if the song is in the song_dictionary and returns false if it is not and returns true if it is.
+def finding(song,song_dictionary):
+    if song in song_dictionary:
         return True
     
     else:
@@ -45,46 +53,48 @@ def finding(song,song_list):
 
 
 #just prints the facts for every song in their playlist
-def display(song_list):
+def display(song_dictionary):
     print("here is a list of all the songs in your playlist:")
-    for item in song_list:
+    for item in song_dictionary:
         print()
         print(item[0], "by", item[1], "made in", item[2])
 
 #Code to update information for each song
-def update(song_list):
+def update(song_dictionary):
     print("songs:")
-    for item in song_list:
+    for item in song_dictionary:
       print(item[0])
     song_to_update = input("What song do you want to update?: ")
 
 
 #the main function that has the most user inputs and has the input to find what they want to do.
 def main():
-    song_list = set()
+    song_dictionary = {
+
+    }
     while True:
         print()
         to_do = input("would you like to add, remove, find, display, update, or leave?: ")
 
         if to_do == "add":
             song = making()
-            song_list = adding(song,song_list)
+            song_dictionary = adding(song,song_dictionary)
 
         elif to_do == "remove":
             song = making()
-            song_list = removing(song,song_list)
+            song_dictionary = removing(song,song_dictionary)
         
         elif to_do == "find":
             song = making()
-            found = finding(song,song_list)
+            found = finding(song,song_dictionary)
             if found == True:
                 print(song[0], "by", song[1], "made in", song[2], "is in your playlist.")
         
         elif to_do == "display":
-            display(song_list)
+            display(song_dictionary)
 
         elif to_do == "update":
-            update(song_list)
+            update(song_dictionary)
         
         elif to_do == "leave":
             break
