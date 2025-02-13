692240b03239a9b602a6d47c08f88992709c2113
Aireos <alex.anderson@ucas-edu.net>
2025-02-13 10:51:08 -0700

    made filters for movies

--- Personal Projects/Movie Recommender/main.py
+++ Personal Projects/Movie Recommender/main.py
@@ -34,5 +34,37 @@ def movies_printer(movie_dictionary):
         print("-------------------------------------------------------")
 
 
+
+def filter_one(movie_dictionary):
+    filter_input_one = input("What is the range you want your movie length to be in? (write like this: 100-120): ")
+    filter_one_movies = []
+    sanitized_one = filter_input_one.split("-")
+    sanitized_one_min = int(sanitized_one[0])
+    sanitized_one_max = int(sanitized_one[1]) + 1
+    for number in range(sanitized_one_min, sanitized_one_max):
+        for movie_name, details in movie_dictionary.items():
+            if number == int(details["length"]):
+                filter_one_movies += {movie_name}
+    return filter_one_movies
+
+
+
+def filter_two(filter_one_movies, movie_dictionary):
+    filter_two_movies = []
+    filter_input_two = input("What is the rating of the movie you want to find? (R, PG-13, or PG): ")
+    for item in filter_one_movies:
+        if movie_dictionary[item]["rating"] == filter_input_two:
+            filter_two_movies.append(item)
+    return filter_two_movies
+
+
+
+def print_filtered(filtered_movies, movie_dictionary):
+    for item in filtered_movies:
+        movie_details = movie_dictionary
+
+
 movie_dictionary = movies_maker()
 movies_printer(movie_dictionary)
+filter_one_movies = filter_one(movie_dictionary)
+filter_two_movies = filter_two(filter_one_movies, movie_dictionary)
