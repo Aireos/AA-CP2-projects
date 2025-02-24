842e4aa85945e90c1b9c0b2bc52c34883b4fd8af
Aireos <alex.anderson@ucas-edu.net>
2025-02-24 10:30:16 -0700

    finished multi page notes

---
+++ Notes/Multi page notes/calcs.py
@@ -0,0 +1,13 @@
+#Alex Anderson, basic math equations for multi page notes
+
+def add(num_one, num_two):
+    return num_one+num_two
+
+def sub(num_one, num_two):
+    return num_one-num_two
+
+def div(num_one, num_two):
+    return num_one/num_two
+
+def mult(num_one, num_two):
+    return num_one*num_two
---
+++ Notes/Multi page notes/main.py
@@ -0,0 +1,10 @@
+#Alex Anderson, multi_page notes
+
+from calcs import add as x
+
+print(x(4,6))
+
+def info():
+    return "Andrew", 35, "orange"
+
+name, age, color = info()
