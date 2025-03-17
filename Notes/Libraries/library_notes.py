7de39b2fb52c266c966f685ede96f55036d43752
aireos <alex.anderson@ucas-edu.net>
2025-03-17 10:39:55 -0600

    started libraries notes

---
+++ Notes/Libraries/library_notes.py
@@ -0,0 +1,51 @@
+
+
+# 1. 
+# What is a library? 
+# A collection of os codes that are used for special features in programs
+
+
+# 2. 
+# What are some standard python libraries?
+# tensorflow
+# matplotlib
+# pandas
+# numpy
+# scipy
+# scrapy
+# pytorch
+# pybrain
+
+
+# 3. 
+# Why do we import other libraries? 
+# So we don't have to make them ourself's
+
+
+# 4. What information do you find in the documentation for a library?
+# How to get the functions and other stuff from the library (mainly uses pip install)
+# Examples
+# Refrences to videos of how to get it to work
+# FAQ
+# Needed info for each function and how they work
+
+
+# 5. 
+# What are good sources for tutorials on a library you have never used before?
+# Their site
+# 
+
+
+# 6. 
+# What is a README file?
+# A document that is saved with your python project and it tells the user the needed info to make it run
+
+
+# 7. 
+# What things belong on a README file?
+# Your python project
+
+
+# 8. 
+# Why do we include README files with our projects?
+# 
