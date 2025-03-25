0b56e876dfdbc54d492cd0033a78c67aa01b0f83
aireos <alex.anderson@ucas-edu.net>
2025-03-25 10:21:38 -0600

    started fixing random character gen

--- Personal Projects/not for class/crazy screen/main.py
+++ Personal Projects/not for class/crazy screen/main.py
@@ -1,5 +1,19 @@
 import random
-characters = ["*","-","_","+","=","`","~","@", "#", "$", "%", "&", "!", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9"]
+# characters = ["*","-","_","+","=","`","~","@", "#", "$", "%", "&", "!", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9"]
+
+import string
+
+# Get all printable ASCII characters
+printable_chars = list(string.printable)
+
+# Convert each character into a quoted string
+quoted_chars = [repr(char) for char in printable_chars]
+
+# Include additional Unicode characters
+unicode_chars = [repr(chr(i)) for i in range(32, 0x110000) if chr(i).isprintable()]
+characters = unicode_chars + quoted_chars
+
+print(characters)
 
 number = -1
 for character in characters:
