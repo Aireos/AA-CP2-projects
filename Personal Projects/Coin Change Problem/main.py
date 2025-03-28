74aa439d6c63b50ce12e91fa981d50882b8e2274
aireos <alex.anderson@ucas-edu.net>
2025-03-28 10:34:23 -0600

    finished basic without errors

--- Personal Projects/Coin Change Problem/main.py
+++ Personal Projects/Coin Change Problem/main.py
@@ -58,7 +58,7 @@ def needed_amounts(denominations, amount):
     sorted_amounts = sorted(amounts, reverse=True)
 
     for item in sorted_amounts:
-
+        print(type(item))
         while True:
             if amount >= item:
                 used.append(item)
@@ -79,11 +79,10 @@ def main():
     print("Welcome to the coin change machiene!")
     country = input("What country's currency are you using (Usa, Europe, Japan, or England): ").strip().capitalize()
     denominations = pull_denominations(country)
-    amount = int(input("What is the amount of money you need?: "))
-    amount = amount*100
+    amount = int(input("What is the amount of money you need?(type without the decimal point, (example: instead of 10.75 do 1075): "))
     used = needed_amounts(denominations, amount)
     
     for i in used:
-        print("You need to have",i,used[i])
+        print("You need to have",used[i],i)
 
 main()
