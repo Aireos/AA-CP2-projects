fa0c5c74c1e06b561b5f18fdafd6a036b05ea23f
aireos <alex.anderson@ucas-edu.net>
2025-03-28 10:46:04 -0600

    finished

--- Personal Projects/Coin Change Problem/main.py
+++ Personal Projects/Coin Change Problem/main.py
@@ -5,9 +5,24 @@ from calculator import *
 
 def main():
     print("Welcome to the coin change machiene!")
-    country = input("What country's currency are you using (Usa, Europe, Japan, or England): ").strip().capitalize()
+
+    countrys = ["Usa", "Europe", "Japan", "England"]
+    while True:
+        country = input("What country's currency are you using (Usa, Europe, Japan, or England): ").strip().capitalize()
+        if country not in countrys:
+            print("that is not a valid country, please choose from the list of countrys.")
+            continue
+        break
+
     denominations = info_pull(country)
-    amount = int(input("What is the amount of money you need?(type without the decimal point, (example: instead of 10.75 do 1075): "))
+    while True:
+        try: 
+            amount = int(input("What is the amount of money you need?(type without the decimal point, (example: instead of 10.75 do 1075): "))
+            break
+        except:
+            print("please follow the guide for what to type")
+            continue
+        
     used = coin_amounts(denominations, amount)
     
     for i in used:
